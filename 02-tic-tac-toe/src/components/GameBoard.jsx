1663eb0142c4e4f6889c3c4b94f97355410fe86a
Rutukoladiya <rutukoladiya@gmail.com>
2025-02-03 15:01:41 +0530

    add tic-tac-toe and investment calc

--- 02-tic-tac-toe/src/components/GameBoard.jsx
+++ 02-tic-tac-toe/src/components/GameBoard.jsx
@@ -1,12 +1,7 @@
 // import { useState } from "react";
 
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({ onSelectSquare}) {
+export default function GameBoard({ onSelectSquare, board }) {
+ // deriving state
 //   const [gameBoard, setGameboard] = useState(initialGameBoard);
 
 //   function handleSelectSquare(rowIndex, colIndex) {
@@ -20,12 +15,12 @@ export default function GameBoard({ onSelectSquare}) {
 
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={onSelectSquare}>{playerSymbol}</button>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
--- 02-tic-tac-toe/src/components/Player.jsx
+++ 02-tic-tac-toe/src/components/Player.jsx
@@ -1,24 +1,40 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol, isActive }) {
-    const [playerName, setPlayerName] = useState(initialName);
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
     // setIsEditing(!isEditing); // it use toggle boolean value ture->false, false->true BUT if our state depends on our prvious state, we should not update the state like this. WHY? coz in this states updates are not performed instantly bt at some point in the future.
     //Insted, pass a fun to our state updating fun. This fun will automatically called by react and will receive the guaranted latest state value
-    setIsEditing(wasEditing => !wasEditing);  
+    setIsEditing((wasEditing) => !wasEditing);
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
   }
 
   function handleChange(event) {
-    setPlayerName(event.target.value)
-  } // called two-way binding
+    setPlayerName(event.target.value);
+  } // called two-way binding - coz we're getting a value out of this input and we're feeding a value back into this input
 
   return (
-    <li className={isActive ? 'active' : undefined}>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         {!isEditing && <span className="player-name">{playerName}</span>}
-        {isEditing && <input type="text" value={playerName} onChange={handleChange} required />}
+        {isEditing && (
+          <input
+            type="text"
+            value={playerName}
+            onChange={handleChange}
+            required
+          />
+        )}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
