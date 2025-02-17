f396e4bdd41f30435a72d6ee3ceb834e8e126277
Rutukoladiya <rutukoladiya@gmail.com>
2025-02-17 11:15:48 +0530

    add some changes

--- PRACTICE/00-week/src/App.jsx
+++ PRACTICE/00-week/src/App.jsx
@@ -1,15 +1,18 @@
 // import './App.css'
-import { Profile } from './components/Week00'
-import ShoppingList from './components/Week01'
-import MyApp from './components/Week02'
+// import { Profile } from './components/Week00'
+// import ShoppingList from './components/Week01'
+// import MyApp from './components/Week02'
+
+import ToDo from "./components/todo/ToDo"
 
 function App() {
 
   return (
     <>
-     <Profile />
+     {/* <Profile />
      <ShoppingList />
-     <MyApp />
+     <MyApp /> */}
+     <ToDo />
     </>
   )
 }
